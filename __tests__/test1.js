d344ecbca82c1e30f07e676fcfa5906c2c1fe460
Belinda Caylor <belinda.caylor@gmail.com>
2020-01-09 14:29:29 -0600

    feat: Setup jest

---
+++ __tests__/test1.js
@@ -0,0 +1,8 @@
+//See this article for instruction.
+//https://www.valentinog.com/blog/jest/
+
+describe("Filter function", () => {
+  test("it should filter by a search term (link)", () => {
+    // actual test
+  });
+});
